8ef8bf0e0a4ab386bcb8b41e8fa7f31a1d408f79
Iris Rifold <irisrifold@gmail.com>
2020-07-20 16:52:52 +0300

    first build

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  "dbURL": "mongodb://localhost:27017",
+  // "dbURL": "mongodb://localhost:27017",
+  "dbURL": "mongodb+srv://ir_b:12345@cluster0.oxrf5.mongodb.net/Cluster0?retryWrites=true&w=majority",
 }
