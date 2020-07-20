53913e6b181b0de7b42beaa6257adba5f5f7ee58
Lee <leedror33@gmail.com>
2020-07-20 20:11:56 +0300

    userlogin

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -4,11 +4,12 @@ const logger = require('../../services/logger.service')
 
 const saltRounds = 10
 
-async function login(email, password) {
-    logger.debug(`auth.service - login with email: ${email}`)
+async function login(userName, password) {
+    logger.debug(`auth.service - login with userName: ${userName}`)
     if (!email || !password) return Promise.reject('email and password are required!')
 
-    const user = await userService.getByEmail(email)
+    const user = await userService.getByUserName(userName)
+    console.log('user-authserv', user);
     if (!user) return Promise.reject('Invalid email or password')
     const match = await bcrypt.compare(password, user.password)
     if (!match) return Promise.reject('Invalid email or password')
