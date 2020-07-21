a57a7ae41c266adea9101d0964e6895448023b4c
Lee <leedror33@gmail.com>
2020-07-21 15:26:10 +0300

    login

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -5,22 +5,16 @@ const logger = require('../../services/logger.service')
 const saltRounds = 10
 
 async function login(userName, password) {
-    logger.debug(`auth.service - login with userName: ${userName}`)
+    // logger.debug(`auth.service - login with userName: ${userName}`)
     if (!userName || !password) return Promise.reject('username and password are required!')
-
     const user = await userService.getByUserName(userName)
-    console.log('user-authserv', user);
     if (!user) return Promise.reject('Invalid username or password')
-    console.log('password', password);
-    console.log(' user.password',  user.password);
-    const hash = await bcrypt.hash('1111', saltRounds)
-    console.log('hash', hash);
     const match = await bcrypt.compare(password, user.password)
-    // const match = await bcrypt.compare('1111', user.password)
-    console.log('match', match);
     if (!match) return Promise.reject('Invalid username or password')
     delete user.password;
-    console.log('user', user);
+    delete user.reviews;
+    delete user.fullName;
+    delete user.bio;
     return user;
 }
 
