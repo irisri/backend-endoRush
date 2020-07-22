0b5758b83a8c4ed5a0ef437a29c837371a11194c
Ofra Bargad <Ofra.bargad@gmail.com>
2020-07-22 14:29:17 +0300

    socket

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -1,22 +1,49 @@
 
 module.exports = connectSockets
+const userIdToSocketMap = {}
 
+function sendMsg(userId,msg){
+    // const socket = userIdToSocketMap[userId]
+    socket.emit('chat addMsg', msg)
+}
 function connectSockets(io) {
     io.on('connection', socket => {
         socket.on('chat newMsg', msg=>{
             console.log(msg)
-            io.emit('chat addMsg', msg)
+            //everyone
+            // io.emit('chat addMsg', msg)
             // emits only to sockets in the same room
-            // io.to(socket.myTopic).emit('chat addMsg', msg)
+            //  io.to(socket.destUser).emit('chat addMsg', msg)
+             socket.emit('chat addMsg', msg)
+
+             io.to(socket.destEvento).emit('chat addMsg', msg)
             // io.emit('chat receivedMsg', msg);
+
+             
         })
-        socket.on('chat topic', topic=>{
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
+        // socket.on('to user', userName=>{
+        //     if (socket.destUser) {
+        //         socket.leave(socket.destUser)
+        //     }
+        //     socket.join(userName)
+        //     socket.destUser = userName;
+        // })
+
+        socket.on('identify', userId=>{
+ 
+            userIdToSocketMap[userId]=socket
+        })
+
+        socket.on('of evento', eventoId=>{
+            if (socket.destEvento) {
+                socket.leave(socket.destEvento)
             }
-            socket.join(topic)
-            socket.myTopic = topic;
+            socket.join(eventoId)
+            console.log('evId',eventoId)
+            socket.destEvento = eventoId;
         })
     })
 }
+
+
 
