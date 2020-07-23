77197a97c3779b51ffe457e61e1ee703ecef1eac
Lee <leedror33@gmail.com>
2020-07-23 20:41:33 +0300

    socet

--- api/socket/socket.routes.js
+++ api/socket/socket.routes.js
@@ -2,46 +2,48 @@
 module.exports = connectSockets
 const userIdToSocketMap = {}
 
-function sendMsg(userId,msg){
-    // const socket = userIdToSocketMap[userId]
-    socket.emit('chat addMsg', msg)
-}
+// function sendMsg(userId,msg){
+//     // const socket = userIdToSocketMap[userId]
+//     socket.emit('chat addMsg', msg)
+// }
+
+// socket.on('identify', userId=>{
+//     userIdToSocketMap[userId]=socket
+// })
+
 function connectSockets(io) {
     io.on('connection', socket => {
-        socket.on('chat newMsg', msg=>{
+        socket.on('chat newMsg', msg => {
             console.log(msg)
             //everyone
             // io.emit('chat addMsg', msg)
             // emits only to sockets in the same room
             //  io.to(socket.destUser).emit('chat addMsg', msg)
-             socket.emit('chat addMsg', msg)
-
-             io.to(socket.destEvento).emit('chat addMsg', msg)
-            // io.emit('chat receivedMsg', msg);
-
-             
-        })
-        // socket.on('to user', userName=>{
-        //     if (socket.destUser) {
-        //         socket.leave(socket.destUser)
-        //     }
-        //     socket.join(userName)
-        //     socket.destUser = userName;
-        // })
-
-        socket.on('identify', userId=>{
- 
-            userIdToSocketMap[userId]=socket
-        })
-
-        socket.on('of evento', eventoId=>{
+            socket.emit('chat addMsg', msg)
+            io.to(socket.destEvento).emit('chat addMsg', msg)
+            // io.emit('chat receivedMsg', msg);   
+        })   
+        socket.on('of evento', eventoId => {
             if (socket.destEvento) {
                 socket.leave(socket.destEvento)
             }
             socket.join(eventoId)
-            console.log('evId',eventoId)
             socket.destEvento = eventoId;
         })
+
+        socket.on('userNewMsg', msg => {
+            console.log('userNewMsg', msg)
+            socket.emit('userMsg', msg)
+            io.to(socket.destUser).emit('userMsg', msg);  
+        })
+        socket.on('to user', ownerId => {
+            if (socket.destUser) {
+                socket.leave(socket.destUser)
+            }
+            socket.join(ownerId)
+            socket.destUser = ownerId;
+        })
+     
     })
 }
 
